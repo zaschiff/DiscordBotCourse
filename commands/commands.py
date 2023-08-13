861e144b37efca530a50847cbd75dcfbe2aebc62
Zach <zaschiff@gmail.com>
2023-08-13 08:56:18 -0600

    completed bot command: SERVER INFO

--- commands/commands.py
+++ commands/commands.py
@@ -27,5 +27,24 @@ async def hello(ctx):
 async def ping(ctx):
     await ctx.send('Pong')
 
+@bot.command()
+async def server(ctx):
+    server_naame        = ctx.guild.name
+    server_dsc          = ctx.guild.description
+    server_region       = ctx.guild.preferred_locale
+    server_icon         = ctx.guild.icon
+    server_member_count = ctx.guild.member_count
+    server_owner        = str(ctx.guild.owner)
 
-bot.run('ADD YOUR SECRET TOKEN IN')
+    server_embed = discord.Embed(
+        title= server_naame + " Server Infromation",
+        description=server_dsc,
+        color=discord.Color.blurple(),
+    )
+    server_embed.add_field(name="Owner", value=server_owner, inline=True)
+    server_embed.add_field(name="Region", value=server_region, inline=True)
+    server_embed.add_field(name="Member Count", value=server_member_count, inline=True)
+
+    await ctx.send(embed=server_embed)
+
+bot.run('YOUR BOT TOKEN')
